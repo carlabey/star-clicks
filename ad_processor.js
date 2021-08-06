f2ca032e26b1fcf27a6133b1cba8006fde1ee2fd
carlabey <74896997+carlabey@users.noreply.github.com>
2021-08-06 21:48:05 +0530

    Create ad_processor.js

---
+++ ad_processor.js
@@ -0,0 +1,10 @@
+//This is used to process ads in the platform
+//1)Get First Ad
+//2)Click Ad
+//3)Go to opened tab
+//3.1)Stay untill page loading
+//3.2)Close the page
+//4)Focus Ad page
+//5)Check Ads
+//5.1)If have ads, go to step 2
+//5.2)If no ads, finish the process.
