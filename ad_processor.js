25bc4201141ef0a2932151177f97d6f0c61b74ae
carlabey <74896997+carlabey@users.noreply.github.com>
2021-08-07 07:26:51 +0530

    added recursion

--- ad_processor.js
+++ ad_processor.js
@@ -8,3 +8,27 @@
 //5)Check Ads
 //5.1)If have ads, go to step 2
 //5.2)If no ads, finish the process.
+let adSelector = document.querySelectorAll("#BasicModulem9_11 > div.panel-body > a");
+
+function processAds(adSelector){
+  let adsCount = adSelector.length;
+  adSelector[0].click();
+  setTimeout(function(){
+    let newAdsCount = adSelector.length;
+    if(adsCount === newAdsCount){
+      location.reload();
+    }else if(newAdsCount === 0){
+      console.log("All ads viewed.");
+    }else{
+      console.log("There are more ads to click");
+      processAds(adSelector);
+    }
+  },5000);
+  //sometimes ad would not load. when this happen add count would not go down even if we click the ad panel.
+  //then we should  reload the page.
+  //using recursion to process the ads.
+}
+
+
+
+
