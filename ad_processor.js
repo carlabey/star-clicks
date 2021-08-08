cfb4687db4ae57c8b5efa0ae1b25d3398239890d
carlabey <74896997+carlabey@users.noreply.github.com>
2021-08-08 08:11:55 +0530

    Update ad_processor.js

--- ad_processor.js
+++ ad_processor.js
@@ -1,3 +1,15 @@
+// ==UserScript==
+// @name         star-clicks ad process
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       carl abey
+// @match        https://www.star-clicks.com/portal/ads
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
 //This is used to process ads in the platform
 //1)Get First Ad
 //2)Click Ad
@@ -8,27 +20,28 @@
 //5)Check Ads
 //5.1)If have ads, go to step 2
 //5.2)If no ads, finish the process.
-let adSelector = document.querySelectorAll("#BasicModulem9_11 > div.panel-body > a");
+    let adSelector = document.querySelectorAll("#BasicModulem9_11 > div.panel-body > a");
 
-function processAds(adSelector){
-  let adsCount = adSelector.length;
-  adSelector[0].click();
-  setTimeout(function(){
-    let newAdsCount = adSelector.length;
-    if(adsCount === newAdsCount){
-      location.reload();
-    }else if(newAdsCount === 0){
-      console.log("All ads viewed.");
-    }else{
-      console.log("There are more ads to click");
-      processAds(adSelector);
-    }
-  },5000);
-  //sometimes ad would not load. when this happen add count would not go down even if we click the ad panel.
-  //then we should  reload the page.
-  //using recursion to process the ads.
-}
+    function processAds(adSelector){
+        let adsCount = adSelector.length;
+        adSelector[0].click();
+        setTimeout(function(){
+            let newAdsCount = adSelector.length;
+            if(adsCount === newAdsCount){
+                console.log("Ads count didnt changed, reload page.");
+                location.reload();
+            }else if(newAdsCount === 0){
+                console.log("All ads viewed.");
+                return;
+            }else{
+                console.log("There are more ads to click");
+                processAds(adSelector);
+            }
+        },5000);
+        //sometimes ad would not load. when this happen add count would not go down even if we click the ad panel.
+        //then we should  reload the page.
+        //using recursion to process the ads.
+    };
 
-
-
-
+    processAds(adSelector);
+})();
